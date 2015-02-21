d38ca7c97960770059fbaddbb19c040a0385cc6b
Ravi Woods <rbw@ic.ac.uk>
2015-02-21 09:07:25 +0000

    added test program

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+	string letter;
+	cin >> letter;
+	if(letter == "r") {
+		cout << "YES" << endl;
+	}
+	return 0;
+}
